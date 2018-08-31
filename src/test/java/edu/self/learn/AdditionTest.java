1d31e7323c5b72ac72251545f03ac7fb7eb97131
aaron b <aaron.beraki@gmail.com>
2018-08-30 22:24:45 -0400

    updated

--- src/test/java/edu/self/learn/AdditionTest.java
+++ src/test/java/edu/self/learn/AdditionTest.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(MockitoJUnitRunner.class)
 public class AdditionTest {
 
     @Test
     public void testAddition() {
-        //        assertEquals(2, Addition.add(1, 1));
+        assertEquals(2, Addition.add(1, 1));
     }
 }
